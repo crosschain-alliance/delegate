5df52e6f471e0d47abfdd0480f235613bc922f19
envin <envin@localhost.localdomain>
2025-03-04 21:22:46 +0100

    feat(contracts): add deploy script

--- contracts/script/LLMAdapter.s.sol
+++ contracts/script/LLMAdapter.s.sol
@@ -9,15 +9,15 @@ contract LLMAdapterDeploy is Script {
 
     function run() public {
         // Use default private key if none provided
-        /*uint256 deployerPrivateKey =
+        uint256 deployerPrivateKey =
             vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
 
         vm.startBroadcast(deployerPrivateKey);
 
-        LLMAdapter adapter = new LLMAdapter();
+        LLMAdapter adapter = new LLMAdapter(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
         console.log("LLMAdapter deployed to:", address(adapter));
 
-        vm.stopBroadcast();*/
+        vm.stopBroadcast();
     }
 }
